c16ecdca92607eedd7e63adf94f3f65f7b8ead31
Zhssam <zzhang.sam@gmail.com>
2023-04-07 21:20:16 +0200

    a

--- elements/idee_random_map.py
+++ elements/idee_random_map.py
@@ -1,61 +1,37 @@
-import pygame
 import random
 
-# Taille de la fenêtre
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+# Dimensions de la carte (sans les murs)
+MAP_WIDTH = 20
+MAP_HEIGHT = 20
 
-# Taille des cases
-CELL_SIZE = 20
+# Probabilité qu'une case soit un mur
+WALL_PROBABILITY = 0.2
 
-# Couleurs
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-BLUE = (0, 0, 255)
+# Dimensions de la carte (avec les murs)
+TOTAL_WIDTH = MAP_WIDTH + 2
+TOTAL_HEIGHT = MAP_HEIGHT + 2
 
-# Initialisation de Pygame
-pygame.init()
+# Matrice représentant la carte
+map = []
 
-# Création de la fenêtre
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# Ajout des murs extérieurs
+for i in range(TOTAL_HEIGHT):
+    if i == 0 or i == TOTAL_HEIGHT - 1:
+        row = [1] * TOTAL_WIDTH
+    else:
+        row = [1] + [0] * MAP_WIDTH + [1]
+    map.append(row)
 
-# Fonction pour générer une carte aléatoire
-def generate_map():
-    map = []
-    for i in range(SCREEN_HEIGHT // CELL_SIZE):
-        row = []
-        for j in range(SCREEN_WIDTH // CELL_SIZE):
-            if random.random() < 0.2:
-                row.append(1)
-            else:
-                row.append(0)
-        map.append(row)
-    return map
+# Remplissage de la matrice avec des murs et des espaces vides
+for i in range(1, TOTAL_HEIGHT - 1):
+    for j in range(1, TOTAL_WIDTH - 1):
+        if random.random() < WALL_PROBABILITY:
+            map[i][j] = 1
+        else:
+            map[i][j] = 0
 
-# Fonction pour dessiner la carte sur l'écran
-def draw_map(map):
-    for i in range(len(map)):
-        for j in range(len(map[i])):
-            if map[i][j] == 1:
-                pygame.draw.rect(screen, BLUE, (j * CELL_SIZE, i * CELL_SIZE, CELL_SIZE, CELL_SIZE))
-
-# Boucle principale du jeu
-running = True
-while running:
-    # Gestion des événements
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                running = False
-    
-    # Génération et affichage de la carte
-    map = generate_map()
-    draw_map(map)
-    
-    # Rafraîchissement de l'écran
-    pygame.display.flip()
-
-# Fermeture de Pygame
-pygame.quit()
+# Affichage de la carte
+for i in range(TOTAL_HEIGHT):
+    for j in range(TOTAL_WIDTH):
+        print(map[i][j], end="")
+    print("")
